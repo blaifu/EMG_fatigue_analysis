271852c66a6f0c40168dab6c5b2d667d2669d23b
Blai Ferrer Uris <blaifu@gmail.com>
2026-09-18 17:19:19 +0200

    Github

--- MUFEE_fatigue_analysis/EMG_fatigue_analysis_EMG_only_streamlit.py
+++ MUFEE_fatigue_analysis/EMG_fatigue_analysis_EMG_only_streamlit.py
@@ -35,7 +35,6 @@ def RMS_calculation (EMG_signal, computation_onsets, computation_offsets):
     muscle_RMS = []
     analysis_window = (computation_offsets[0] - computation_onsets[0])
     for i in range(0,len(computation_onsets)):
-        st.write(f'onset: {computation_onsets[i]}; offset: {computation_offsets[i]}')
         muscle_RMS.append ((np.sum(np.square(EMG_signal[computation_onsets[i]:computation_offsets[i]]))/analysis_window)**0.5)
     return muscle_RMS
 def mean_median_frequency_computation (EMG_onsets, EMG_offsets, data_frame_nAbs, muscle):
